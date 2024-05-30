3e4e682647a656c675623ba3cc2922e63e2d124a
Pau <113383428+daw2-diaz23@users.noreply.github.com>
2024-05-30 13:52:23 +0000

    Historia 2

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,15 @@
-// main.tsx or main.jsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {NextUIProvider} from '@nextui-org/react'
+import { NextUIProvider } from '@nextui-org/react'
 import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <NextUIProvider>
-      <App />
+      <main className="text-foreground bg-background">
+        <App />
+      </main>
     </NextUIProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
